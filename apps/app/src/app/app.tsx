2ced3fd7cdaca271a941c20cc060f46dac443168
justalk <justal.kevin@gmail.com>
2023-07-05 20:55:03 +0000

    [FEATURE] Installing the filepond

--- apps/app/src/app/app.tsx
+++ apps/app/src/app/app.tsx
@@ -1,12 +1,29 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useState } from 'react';
 import styles from './app.module.scss';
+import { FilePond, registerPlugin } from 'react-filepond';
+import { FilePondInitialFile, FilePondFile } from 'filepond/types/index';
 
-import NxWelcome from './nx-welcome';
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+
+// Import FilePond styles
+import 'filepond/dist/filepond.min.css';
+
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
 export function App() {
+  const [files] = useState([]);
   return (
     <div>
-      <NxWelcome title="app" />
+      <FilePond
+        files={files}
+        allowMultiple={true}
+        maxFiles={3}
+        server="/api"
+        name="files" /* sets the file input name, it's filepond by default */
+        labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+      />
     </div>
   );
 }
