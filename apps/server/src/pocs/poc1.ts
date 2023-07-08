70952ce58538a61d61295fd86b7933a38ec2e359
justalk <justal.kevin@gmail.com>
2023-07-08 09:15:07 +0000

    [FEATURE] Adding pintura with filepond

--- apps/server/src/pocs/poc1.ts
+++ apps/server/src/pocs/poc1.ts
@@ -9,12 +9,17 @@ interface MulterRequest extends Request {
 
 router.post(
   '/upload',
-  multer({ dest: 'uploads/poc1' }).single('documents'),
+  multer({ dest: 'uploads/poc1' }).single('files'),
   (req: Request, res) => {
     const files = (req as MulterRequest).file;
     console.log(files);
     res.send({ message: 'Welcome to server!' });
   }
 );
+
+router.delete('/upload', (req: Request, res) => {
+  console.log(req);
+  res.send({ message: 'Welcome to server!' });
+});
 
 module.exports = router;
